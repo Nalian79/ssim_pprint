82a89977f22e48fd4752ec44e80e8c85e19bbaf0
Michele <michele.morelock@gmail.com>
2015-02-12 22:40:57 +0000

    Added function to test if file is compressed.

--- ssim_pprint.py
+++ ssim_pprint.py
@@ -3,52 +3,64 @@ import logging
 import os
 import sys
 
-from utils import convert_to_text
+from utils import is_file_compressed, uncompress
 
 # Set the log output file and log level.
-logging.basicConfig(filename="ssim_pprint.log", level=logging.DEBUG)
+logging.basicConfig(filename='ssim_pprint.log', level=logging.DEBUG)
 
 
 def make_parser():
-    """ Construct the command line parser """
-    logging.debug("Constructing command line parser...")
-    description = "Display information about a specific Airline Flight"
+    """ Construct the command line parser."""
+
+    logging.debug('Constructing command line parser...')
+    description = 'Display information about a specific Airline Flight'
     parser = argparse.ArgumentParser(description=description)
 
-    subparsers = parser.add_subparsers(dest="command",
-                                       help="Available Commands")
+    subparsers = parser.add_subparsers(dest='command',
+                                       help='Available Commands')
 
     #Todo: Consider supporting a mapping of names to IATA codes.
-    logging.debug("Constructing carrier subparser")
-    carrier_parser = subparsers.add_parser("carrier", help="")
-#    carrier_parser.add_argument("code", help="Two digit IATA carrier code.")
-#    carrier_parser.add_argument("flight", help="The flight number to look up.")
-    carrier_parser.add_argument("filename", help="Name of the file to search.")
-
+    logging.debug('Constructing the flight lookup subparser')
+    lookup_parser = subparsers.add_parser('lookup',
+                                          help='Look up specific flight data.')
+    lookup_parser.add_argument('-c', '--carrier',
+                               help='Two digit IATA carrier code')
+    lookup_parser.add_argument('-f', '--flight',
+                               help='Number of the flight to lookup')
+    lookup_parser.add_argument('-s', '--ssim',
+                               help='Name of the SSIM file to search')
     return parser
 
+
 def main():
-    """ Main function """
+    """ Parse command line args, take action based on arguments."""
+
     logging.info("Starting the SSIM pretty printer.")
     parser = make_parser()
     logging.debug("All arguments are: {!r}".format(sys.argv[0:]))
-    arguments = parser.parse_args(sys.argv[1:])
+    args = parser.parse_args(sys.argv[1:])
     # Convert parsed arguments from Namespace to dictionary
 
-    arguments = vars(arguments)
-    logging.debug("Arguments are now: {!r}".format(arguments))
-    command = arguments.pop("command")
+    args = vars(args)
+    logging.debug("Arguments are now: {!r}".format(args))
+    command = args.pop("command")
     # Log an error and quit if file doesn't exist
-    if os.path.exists(arguments['filename']):
-        logging.debug("File {!r} exists - Continuing.".format(
-                arguments['filename']))
-    else:
-        logging.debug("File {!r} doesn't exist.  Exiting.".format(
-                arguments['filename']))
-        sys.exit("SSIM file name provided not found.")
 
-    if command == "carrier":
-        file_to_parse = convert_to_text(arguments['filename'])
+    if command == "lookup":
+        if os.path.exists(args['ssim']):
+            logging.debug('File {!r} exists, Continuing.'.format(
+                    args['ssim']))
+        else:
+            logging.debug('File {!r} doesn\'t exist.  Exiting'.format(
+                    arguments['ssim']))
+            sis.exit('SSIM file name provided not found.')
+
+        filename = args['ssim']
+        if is_file_compressed(filename):
+            file_to_parse = uncompress(filename)
+        else:
+            file_to_parse = filename
+            pass
         print("Check file {!r}".format(file_to_parse))
 
 
--- utils.py
+++ utils.py
@@ -1,19 +1,47 @@
 import gzip
 
-def convert_to_text(gzfilename):
+
+def is_file_compressed(filename):
+    """ Determine if the file we are dealing with is compressed with gzip.
+
+    Args:
+      filename: name of the file to check.
+    Returns:
+      A string indicating the type of file.
+    """
+
+    magic_dict = {
+        "\x1f\x8b\x08": "gz",
+        "\x50\x4b\x03\x04": "zip"
+        }
+
+    max_len = max(len(x) for x in magic_dict)
+
+    with open(filename) as f:
+        file_start = f.read(max_len)
+        for magic, filetype in magic_dict.items():
+            if file_start.startswith(magic):
+                return True, filetype
+        return False
+
+
+def uncompress(filename):
     """Uncompress the gzipped file, return the uncompressed file name.
 
-    Read in the uncompressed file, and write out a new file, uncompressed,
+    Read in the compressed file, and write out a new file, uncompressed,
     with the same name, minus .gz.
 
     args: gzfilename - the name of the gzipped file you want to uncompress.
     """
 
-    print gzfilename
-    new_name = gzfilename.rstrip('.gz')
-    f_in = gzip.open(gzfilename, 'rb')
-    f_out = open(new_name, 'wb')
-    f_out.writelines(f_in)
-    f_out.close()
-    f_in.close()
-    return new_name
+    print('Incoming file is {!r}'.format(filename))
+    if is_file_compressed(filename):
+        new_name = filename.rstrip('.gz')
+        f_in = gzip.open(filename, 'rb')
+        f_out = open(new_name, 'wb')
+        f_out.writelines(f_in)
+        f_out.close()
+        f_in.close()
+        return new_name
+    else:
+        return filename
